533b2ba70b969cd85dc31acf795cb1073af28ff1
Your Name <you@example.com>
2022-04-30 13:40:23 +0530

    [Megha] Add: Third Position Of String Value Is Greatest

---
+++ TestMaximum/UnitTest1.3.cs
@@ -0,0 +1,21 @@
+﻿using GenericProblems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class UnitTest2
+    {
+        [Test]
+        public void GivenStringInput_WhenTestMax_ThirdPosition_ShouldReturnMaxNum()
+        {
+            FindMaximum max = new FindMaximum();
+            string actual = max.FindMaxString("Banana","Peach", "Apple");
+            Assert.AreEqual(actual, "Banana");
+        }
+    }
+}
