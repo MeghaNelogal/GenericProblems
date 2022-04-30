4446098a7f85a7845326a9941c58d0d0cf99e980
Your Name <you@example.com>
2022-04-30 15:47:38 +0530

    [Megha] Add: Finding Greatest String Value Is Done

---
+++ TestMaximum/UnitTest1.3.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GenericProblems.FindMaximum;
+
+namespace TestMaximum
+{
+    public class UnitTest2
+    {
+        public void GivenFloatInput_WhenTestMax_ThirdPosition_ShouldReturnMaxNum()
+        {
+            GenericMaximum<string> max = new GenericMaximum<string>();
+            string[] array = { "Peach", "Apple", "Banana", "Mango", "PineApple", "Graps" };
+            string actual = max.FindMaxElement(array);
+            Assert.AreEqual(actual, "PineApple");
+        }
+    }
+}
+
