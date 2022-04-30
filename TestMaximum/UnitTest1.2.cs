0aebdfcb0a6b0b44a85a57b66935384079a04613
Your Name <you@example.com>
2022-04-30 15:43:16 +0530

    [Megha] Add: Finding Greatest Float Value Is Done

---
+++ TestMaximum/UnitTest1.2.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GenericProblems.FindMaximum;
+
+namespace TestMaximum
+{
+    public class UnitTest1
+    {
+        public void GivenStringInput_WhenTestMax_SecondPosition_ShouldReturnMaxNum()
+        {
+            GenericMaximum<float> max = new GenericMaximum<float>();
+            float[] array = { 10.5f, 11.5f, 12.5f, 15.8f, 99.33f };
+            float actual = max.FindMaxElement(array);
+            Assert.AreEqual(actual, 99.33f);
+        }
+    }
+}
