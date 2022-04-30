b897030c98136dc31c69d96b2093cb5b26a964d0
Your Name <you@example.com>
2022-04-30 12:26:15 +0530

    [Megha] Add: Third Position Element Is Greatest

---
+++ TestMaximum/UnitTest1.3.cs
@@ -0,0 +1,22 @@
+﻿using GenericProblems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestMaximum
+{
+    public class UnitTest2
+    {
+        [Test]
+        public void Test3()
+        {
+            FindMaximum max = new FindMaximum();
+            int value = max.MaximumElement(2, 14, 44);
+            Assert.AreEqual(value, 44);
+        }
+    }
+}
